5b7dbe3ea663ecf43ec56f6fbf7a332af293fadf
Andrew Liu <aliu730@gmail.com>
2018-10-25 19:37:08 -0700

    merging the component into dataEntry

--- client/src/components/dataEntry.jsx
+++ client/src/components/dataEntry.jsx
@@ -13,8 +13,8 @@ const DataEntry = (props) => {
       <div className="register" >
         <div className="leftEntry">
           <div className="headBoard">New Entry</div>
-          <input className="dateSelect" onChange={ this.getDate } type="date" defaultValue={`${year}-${month}-${day}`}/>
-          <GameSelect getGame={ this.getGame } games={ games } />
+          <input className="dateSelect" onChange={ props.getDate } type="date" defaultValue={`${year}-${month}-${day}`}/>
+          <GameSelect getGame={ props.getGame } games={ props.games } />
           <PlayerSelect className="playerSelector" addPlayer={ this.addPlayer } getName={ this.getName } displayPlayers={ displayPlayers } />
           <div className="currPlayers">
             Current Players:
@@ -23,40 +23,8 @@ const DataEntry = (props) => {
             }
           )}
           </div>
-          <div className="winner">
-            Winner:&nbsp;
-            <select onChange={this.getWinner} className="winnerSelect" defaultValue="selectWinner" >
-              <option value="selectWinner">
-                Pick a Winner
-              </option>
-              {players.map((player, i) => (
-                <option value={player} key={ i } >
-                  { player }
-                </option>
-              )
-            )}
-            </select>
-          </div>
-          <button onClick={this.submit} className="submit">Submit</button>
-          <button onClick={this.enterEntry} className="showStats">Cancel</button>
-        </div>
-        <div className="rightEntry">
-          <div className="headBoard">New Players/Games</div>
-          <div className="newPlayer">
-            New Player:&nbsp;
-            <input onChange={this.newPlayer} className="addForm" type="text"></input>
-            <button onClick={this.newPlayerAdd} className="addPlayer">+</button>
-          </div>
-          <div className="newGame">
-            New Game:&nbsp;
-            <input  onChange={this.gameChangeAdd} className="addForm" type="text" ></input>
-            <button onClick={this.addGame} className="addGame">+</button>
-          </div>
-          <div className="filler-div">
-              
-          </div>
-        </div>
       </div>
+    </div>
     </div>
   )
 };
